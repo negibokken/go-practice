e8d641be7db76a097e4f777c42b9599e1736a577
negibokken <negibokken@gmail.com>
2020-07-10 22:17:19 +0900

    Update by using template

--- go-webapp/main.go
+++ go-webapp/main.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-	<html>
-		<head>
-			<title>チャット</title>
-		</head>
-		<body>
-			チャットしましょう！
-		</body>
-	</html>
-`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	fmt.Println("Listening :8080!")
 	if err := http.ListenAndServe("localhost:8080", nil); err != nil {
