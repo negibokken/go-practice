acdbaa10f83e9ccb39b95d73a5f400b2d47d2792
negibokken <negibokken@gmail.com>
2020-07-11 10:18:19 +0900

    Add room & client

--- go-webapp/main.go
+++ go-webapp/main.go
@@ -7,7 +7,62 @@ import (
 	"net/http"
 	"path/filepath"
 	"sync"
+
+	"github.com/gorilla/websocket"
 )
+
+type room struct {
+	forward chan []byte
+	join    chan *client
+	leave   chan *client
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			r.clients[client] = true
+		case client := <-r.leave:
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+				default:
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
+}
+
+type client struct {
+	socket *websocket.Conn
+	send   chan []byte
+	room   *room
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
 
 type templateHandler struct {
 	once     sync.Once
