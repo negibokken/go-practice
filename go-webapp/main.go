f62f28996ca18e6cb3346bc6f2f8a51484e911c9
negibokken <negibokken@gmail.com>
2020-07-12 18:13:12 +0900

    Add simple login page

--- go-webapp/main.go
+++ go-webapp/main.go
@@ -125,7 +125,8 @@ func main() {
 	flag.Parse()
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
-	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
 	http.Handle("/room", r)
 	go r.run()
 
