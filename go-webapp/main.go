ca34b07dc92cb12aebb9fa4e5aa0caf1b02acefd
negibokken <negibokken@gmail.com>
2020-07-19 00:29:18 +0900

    Update method for getting avatar

--- go-webapp/main.go
+++ go-webapp/main.go
@@ -19,7 +19,11 @@ import (
 	"github.com/stretchr/objx"
 )
 
-var avatars Avatar = UseFileSystemAvatar
+var avatars Avatar = TryAvatars{
+	UseFileSystemAvatar,
+	UseAuthAvatar,
+	UseGravatar,
+}
 
 const (
 	socketBufferSize  = 1024
